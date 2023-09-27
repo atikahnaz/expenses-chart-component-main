747d0d32dfb033a9d8c09d824d103d9ba5f39a85
Atikah <atikahnazir6@gmail.com>
2023-09-27 14:45:21 +0800

    bar chart

--- chart1.js
+++ chart1.js
@@ -1,22 +1,73 @@
 const ctx = document.getElementById("myChart");
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((json) => {
+    const day = json.map((item) => item.day);
+    const amount = json.map((item) => item.amount);
+    //console.log(day);
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: day,
+        datasets: [
+          {
+            label: "# of Votes",
+            data: amount,
+            backgroundColor: "hsl(10, 79%, 65%)",
+            hoverBackgroundColor: "hsl(186, 34%, 60%)",
+            borderRadius: 3,
+            borderSkipped: false,
+          },
+        ],
       },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+      options: {
+        layout: {
+          padding: {},
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            yAlign: "bottom",
+            caretSize: 0,
+            caretPadding: 5,
+            displayColors: false,
+            callbacks: {
+              title: function () {
+                return ""; // Remove the title (x-axis data)
+              },
+              label: function (context) {
+                const value = context.parsed.y;
+                return `$${value}`;
+              },
+            },
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false,
+              offset: true,
+            },
+            border: {
+              display: false,
+            },
+          },
+          y: {
+            beginAtZero: true,
+            grid: {
+              display: false,
+            },
+            border: {
+              display: false,
+            },
+            ticks: {
+              display: false,
+            },
+          },
+        },
       },
-    },
-  },
-});
+    });
+  });
